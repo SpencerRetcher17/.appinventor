eedad7d2d3eec37cb370a5a705a9d1035719a4d9
SpencerRetcher17 <retchers@bishopireton.org>
2017-04-09 16:11:51 -0400

    Update

--- core/src/com/mygdx/game/GameWorld.java
+++ core/src/com/mygdx/game/GameWorld.java
@@ -2,86 +2,109 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector3;
 
-public class GameWorld extends ApplicationAdapter {
-	SpriteBatch batch;
+
+
+public class GameWorld extends ApplicationAdapter implements InputProcessor {
+
+
 	Texture img;
+	TiledMap tiledMap;
 	OrthographicCamera camera;
-	Rectangle head;
-	float time;
-	boolean left;
-	boolean right=true;
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("Arcade-Centipede-Centipede_03.png");
+	TiledMapRenderer tiledMapRenderer;
+	SpriteBatch sb;
+	Texture texture;
+	Sprite sprite;
 
-		// create a Rectangle to logically represent the bucket
-		head = new Rectangle();
-		head.x = 800 / 2 - 64 / 2; // center the bucket horizontally
-		head.y = 20; // bottom left corner of the bucket is 20 pixels above
-		// the bottom screen edge
-		head.width = 60;
-		head.height = 30;
+	@Override public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 
-
-		 camera = new OrthographicCamera();
-		camera.setToOrtho(true, 800, 480);
-
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		// tell the camera to update its matrices.
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,256,256);
 		camera.update();
+		tiledMap = new TmxMapLoader().load("custom.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		Gdx.input.setInputProcessor(this);
 
-		// tell the SpriteBatch to render in the
-		// coordinate system specified by the camera.
-		batch.setProjectionMatrix(camera.combined);
-
-		batch.begin();
-		batch.draw(img, head.x, head.y,head.width,head.height);
-		batch.end();
-
-		head.x-=200*Gdx.graphics.getDeltaTime();
-
-		/*
-		if(head.x>0&&left==true)
-		{
-			time =200 * Gdx.graphics.getDeltaTime();
-			left=true;
-			head.x-=time;
-		}
-
-		if (head.x <=0)
-		{
-			time =200 * Gdx.graphics.getDeltaTime();
-			left=false;
-			head.x+=time;
-		}
-		 if(head.x>=800)
-		{
-			time =200 * Gdx.graphics.getDeltaTime();
-			left=true;
-			head.x-=time;
-		}
-		*/
-
-
-
-
+		sb = new SpriteBatch();
+		//texture = new Texture(Gdx.files.internal("badlogic.jpg"));
+		//sprite = new Sprite(texture);
 	}
-	
+
+	@Override public void render () {
+
+		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+		sb.setProjectionMatrix(camera.combined);
+		sb.begin();
+		//sprite.draw(sb);
+		sb.end();
+	}
+
+
+	@Override public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override public boolean keyUp(int keycode) {
+		if(keycode == Input.Keys.LEFT)
+			camera.translate(-32,0);
+		if(keycode == Input.Keys.RIGHT)
+			camera.translate(32,0);
+		if(keycode == Input.Keys.UP)
+			camera.translate(0,-32);
+		if(keycode == Input.Keys.DOWN)
+			camera.translate(0,32);
+		if(keycode == Input.Keys.NUM_1)
+			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+		if(keycode == Input.Keys.NUM_2)
+			tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
+		return false;
+	}
+
+	@Override public boolean keyTyped(char character) {
+
+		return false;
+	}
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		Vector3 clickCoordinates = new Vector3(screenX,screenY,0);
+		Vector3 position = camera.unproject(clickCoordinates);
+		sprite.setPosition(position.x, position.y);
+		return true;
+	}
+
+	@Override public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override public boolean scrolled(int amount) {
+		return false;
 	}
 }
